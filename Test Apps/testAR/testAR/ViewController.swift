c9091f593bceab22005074f6ec80dbe16c4ea20d
svgrossarth <svgrossarth@ucdavis.edu>
2020-02-13 20:11:22 -0800

    working on debugging

--- Test Apps/testAR/testAR/ViewController.swift
+++ Test Apps/testAR/testAR/ViewController.swift
@@ -12,6 +12,7 @@ import RealityKit
 let WORLD_ORIGIN : String = "worldOrigin"
 let PREVIOUS_POINT : String = "perviousPoint"
 let CURRENT_POINT : String = "currentPoint"
+let concurrentQueue = DispatchQueue(label: "com.queue.Concurrent", attributes: .concurrent)
 
 class ViewController: UIViewController {
     
@@ -19,6 +20,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        arView.renderOptions = [.disableDepthOfField, .disableCameraGrain, .disableMotionBlur, .disableFaceOcclusions, .disablePersonOcclusion, .disableGroundingShadows, .disableAREnvironmentLighting]
         
         //this anchor is at the origin world origin, it can be used a refence for other entities
         var worldOrigin = AnchorEntity(world: [0,0,0])
@@ -52,6 +54,7 @@ extension ARView {
                 print("cant get anchor")
                 return
             }
+            //let material = UnlitMaterial()
             let material = UnlitMaterial(color: .green)
             //let material = SimpleMaterial(color: .green, isMetallic: false)
             //let entity = ModelEntity(mesh: MeshResource.generateSphere(radius: 0.01), materials: [material])
@@ -71,54 +74,58 @@ extension ARView {
     
     override open func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?){
         //print("touch moved")
-        if let singleTouch = touches.first{
-            var touchLocation = singleTouch.location(in: self)
-            var cameraTran = self.cameraTransform
-            //this is important, or else the plan is off
-            cameraTran.rotation = simd_quatf(angle: .pi/2 , axis: simd_float3(x: 1, y: 0, z: 0) )
-            //puts the points in front of camera
-            cameraTran.translation = SIMD3<Float>(cameraTran.translation.x, cameraTran.translation.y, cameraTran.translation.z - 0.3)
+        //concurrentQueue.async {
+        print("touch moved")
             
-            guard let pointIn3d = self.unproject(touchLocation, ontoPlane: cameraTran.matrix) else {
-                print("can't unproject")
-                return
+            if let singleTouch = touches.first{
+                var touchLocation = singleTouch.location(in: self)
+                var cameraTran = self.cameraTransform
+                //this is important, or else the plan is off
+                cameraTran.rotation = simd_quatf(angle: .pi/2 , axis: simd_float3(x: 1, y: 0, z: 0) )
+                //puts the points in front of camera
+                cameraTran.translation = SIMD3<Float>(cameraTran.translation.x, cameraTran.translation.y, cameraTran.translation.z - 0.3)
+                
+                guard let pointIn3d = self.unproject(touchLocation, ontoPlane: cameraTran.matrix) else {
+                    print("can't unproject")
+                    return
+                }
+                guard let anchor = self.scene.anchors.first else {
+                    print("cant get anchor")
+                    return
+                }
+                guard let previousPoint = self.scene.findEntity(named: PREVIOUS_POINT) as? ModelEntity else {
+                    print("can't find previous point")
+                    return
+                }
+                let cloneEntity = previousPoint.clone(recursive: false)
+                //            let material = SimpleMaterial(color: .green, isMetallic: false)
+                //            let entity = ModelEntity(mesh: MeshResource.generateBox(width: 0.01, height: 0.01, depth: 0.01, cornerRadius: 1, splitFaces: false), materials: [material])
+                //this is vital this realativeTo part is relative to the anchor at 0,0,o so the coordinates map properly
+                cloneEntity.setPosition(pointIn3d, relativeTo: anchor)
+                
+                guard let worldOrigin = self.scene.findEntity(named: WORLD_ORIGIN) else {
+                    print("can't find world origin")
+                    return
+                }
+                
+                self.generateMorePoints(currentPoint: cloneEntity, previousPoint: previousPoint, worldOrigin: worldOrigin)
+                
+                
+                anchor.addChild(cloneEntity)
+                previousPoint.name = ""
+                //print("new previous point")
+                cloneEntity.name = PREVIOUS_POINT
+            } else {
+                print("can't get touch")
             }
-            guard let anchor = self.scene.anchors.first else {
-                print("cant get anchor")
-                return
-            }
-            guard let previousPoint = self.scene.findEntity(named: PREVIOUS_POINT) as? ModelEntity else {
-                print("can't find previous point")
-                return
-            }
-            let cloneEntity = previousPoint.clone(recursive: false)
-//            let material = SimpleMaterial(color: .green, isMetallic: false)
-//            let entity = ModelEntity(mesh: MeshResource.generateBox(width: 0.01, height: 0.01, depth: 0.01, cornerRadius: 1, splitFaces: false), materials: [material])
-            //this is vital this realativeTo part is relative to the anchor at 0,0,o so the coordinates map properly
-            cloneEntity.setPosition(pointIn3d, relativeTo: anchor)
-
-            guard let worldOrigin = self.scene.findEntity(named: WORLD_ORIGIN) else {
-                print("can't find world origin")
-                return
-            }
-
-            self.generateMorePoints(currentPoint: cloneEntity, previousPoint: previousPoint, worldOrigin: worldOrigin)
-            
-
-            anchor.addChild(cloneEntity)
-            previousPoint.name = ""
-            //print("new previous point")
-            cloneEntity.name = PREVIOUS_POINT
-        } else {
-            print("can't get touch")
-        }
+        //}
     }
     
     func generateMorePoints(currentPoint : ModelEntity, previousPoint : ModelEntity, worldOrigin : Entity){
         let currentPointPosition = currentPoint.position(relativeTo: worldOrigin)
         let previousPointPosition = previousPoint.position(relativeTo: worldOrigin)
         let distance = distanceBetweenPoints(currentPointPosition: currentPointPosition, previousPointPosition: previousPointPosition)
-        if(distance > 0.005){
+        if(distance > 0.05){
             renderPoints(currentPointPosition: currentPointPosition, previousPointPosition: previousPointPosition, point : previousPoint, worldOrigin : worldOrigin, distance: distance, model: currentPoint)
         }
     }
@@ -132,42 +139,65 @@ extension ARView {
     }
     
     func renderPoints(currentPointPosition: simd_float3, previousPointPosition: simd_float3, point: Entity, worldOrigin : Entity, distance : Float, model : ModelEntity){
-        //let lineBetweenPoints = currentPointPosition - previousPointPosition
-        let newDistance = distance + 0.01
-        let midPoint = simd_float3(x: (currentPointPosition.x + previousPointPosition.x)/2, y: (currentPointPosition.y + previousPointPosition.y)/2, z: ((currentPointPosition.z + previousPointPosition.z)/2))
+        print("rending points")
+        print(previousPointPosition)
+        let lineBetweenPoints = currentPointPosition - previousPointPosition
+        //let newDistance = distance + 0.01
+        //let midPoint = simd_float3(x: (currentPointPosition.x + previousPointPosition.x)/2, y: (currentPointPosition.y + previousPointPosition.y)/2, z: ((currentPointPosition.z + previousPointPosition.z)/2))
 
-        let material = UnlitMaterial(color: .green)
+        //let material = UnlitMaterial(color: .green)
         //let material = SimpleMaterial(color: .green, isMetallic: false)
         //debugging
         //let connectingEntity = ModelEntity(mesh: MeshResource.generateBox(width: distance, height: 0.001, depth: 0.001, cornerRadius: 1, splitFaces: false), materials: [material])
         //normal
-        let connectingEntity = ModelEntity(mesh: MeshResource.generateBox(width: newDistance, height: 0.01, depth: 0.01, cornerRadius: 1, splitFaces: false), materials: [material])
-        connectingEntity.setPosition(midPoint, relativeTo: worldOrigin)
-        connectingEntity.transform.rotation = findRotation(currentPointPosition: currentPointPosition, previousPointPosition: previousPointPosition, distance: distance)
-        worldOrigin.addChild(connectingEntity)
+//        let connectingEntity = ModelEntity(mesh: MeshResource.generateBox(width: newDistance, height: 0.01, depth: 0.01, cornerRadius: 1, splitFaces: false), materials: [material])
+//        connectingEntity.setPosition(midPoint, relativeTo: worldOrigin)
+//        connectingEntity.transform.rotation = findRotation(currentPointPosition: currentPointPosition, previousPointPosition: previousPointPosition, distance: distance)
+//        worldOrigin.addChild(connectingEntity)
        // print(worldOrigin.children.count)
         
         
         
         
         
-        //let smallStep : Float = 0.001
-       // let midPoint = simd_float3(x: (currentPointPosition.x - previousPointPosition.x)/2, y: (currentPointPosition.y - previousPointPosition.y)/2, z: ((currentPointPosition.z - previousPointPosition.z)/2)  - 0.3)
-        //let num = Int(distance/smallStep)
+        let smallStep : Float = 0.01
+        //let midPoint = simd_float3(x: (currentPointPosition.x - previousPointPosition.x)/2, y: (currentPointPosition.y - previousPointPosition.y)/2, z: ((currentPointPosition.z - previousPointPosition.z)/2)  - 0.3)
+        let num = Int(distance/smallStep)
         //print(num)
+        print("num is")
+        print(num)
 
 //        let ball = MeshResource.generateSphere(radius: 0.01)
-        
-//        for i in 1...num {
-//            let clone = model.clone(recursive: false)
-//            //let entity = ModelEntity(mesh: ball, materials: [material])
-//            //let smallMove = lineBetweenPoints * (smallStep * Float(i))
-//            let smallMove = lineBetweenPoints * (0.001 * Float(i))
-//            clone.setPosition(smallMove, relativeTo: point)
-//           // print(entity.position)
-//            worldOrigin.addChild(clone)
-//            print(worldOrigin.children.count)
-//        }
+        let material = UnlitMaterial(color: .red)
+        let box = MeshResource.generateBox(width: 0.004, height: 0.004, depth: 0.004, cornerRadius: 1, splitFaces: false)
+        for i in 1...num {
+          // let testMod =  ModelEntity(mesh: box, materials: [material])
+            
+            print("in for loop this iteration")
+            print(i)
+            let clone = model.clone(recursive: false)
+            clone.model?.materials = [material]
+            clone.model?.mesh = box
+            
+            //let entity = ModelEntity(mesh: ball, materials: [material])
+            //let smallMove = lineBetweenPoints * (smallStep * Float(i))
+            //let smallMove = lineBetweenPoints * (0.01 * Float(i))
+            let test = Float(i/num)
+            print(previousPointPosition + lineBetweenPoints * test)
+            print(currentPointPosition)
+//            print(previousPointPosition)
+//            print(smallMove + previousPointPosition)
+            clone.setPosition(previousPointPosition + lineBetweenPoints * test, relativeTo: worldOrigin)
+           // testMod.setPosition(previousPointPosition + lineBetweenPoints * test, relativeTo: worldOrigin)
+           // print(entity.position)
+           // worldOrigin.addChild(clone)
+            //model.setPosition(smallMove, relativeTo: point)
+           // worldOrigin.addChild(model)
+            worldOrigin.addChild(clone)
+           // worldOrigin.addChild(testMod)
+                print("num children")
+                 print(worldOrigin.children.count)
+        }
 
         
     }
@@ -204,7 +234,6 @@ extension ARView {
             oppositeSide = previousPointPosition.y - currentPointPosition.y
             angle = 2 * .pi -  asin(oppositeSide/distance)
             //angle = asin(oppositeSide/distance)
-            print("hi")
         }
 
         return simd_quatf(angle: angle, axis: simd_float3(x: 0, y: 0, z: 1))
